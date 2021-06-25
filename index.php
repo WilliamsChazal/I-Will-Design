f9d45e4d8c57b6303e24c477c76b275d64656c7d
williams <w.chazal@codeur.online>
2021-06-25 10:22:51 +0200

    creation des sections à inclure

--- index.php
+++ index.php
@@ -10,49 +10,49 @@
 <?php include ('header.php')?>
 <?php include ('includes/navbar/navbar.php')?>
 
-<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
+        <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 
-    <section class='main'>
-        </div>
-            <div class='main_text'>
-                <div class='main_text--aera'>
-                     <h1 class='bounce-in-left'>Williams Chazal</h1>
-                    <div class='bounce-in-left'> <h3 class="main_text--slogan">"Des idées au pluriel"</h3></div>
-                    <div class="main_text--metier"><h2 class='bounce-in-right'>WebDesign  <span id="blue_text">Dev'Web</span></h2></div>
-                        <a href="#projet_ancre"><div class="main_text--button">
-                        Portfolio </div></a>
+            <section class='main'>
+
+                    <div class='main_text'>
+                        <div class='main_text--aera'>
+                            <h1 class='bounce-in-left'>Williams Chazal</h1>
+                            <div class='bounce-in-left'> <h3 class="main_text--slogan">"Des idées au pluriel"</h3></div>
+                            <div class="main_text--metier"><h2 class='bounce-in-right'>WebDesign  <span id="blue_text">Dev'Web</span></h2></div>
+                                <a href="#projet_ancre"><div class="main_text--button">
+                                Portfolio </div></a>
+                        </div>
+                    </div>
+                    </div> 
+            </section>
+            <section class="about-me" id="about-me_ancre">
+                <div class="about-me_information">
+                    <h2>Qui suis je ?</h2>
+                    <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
+                    Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p>
+                        <div class="about-me_information--button">
+                            <div class="about-me_button">Curriculum Vitae</div> 
+                        </div>    
                 </div>
-            </div>
+                <div class="about-me_photo">
+                    <img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait">
+                </div>
+                
+            </section>
+            <section ><?php include ('includes/skills/skills.php')?></section>
 
-    </section>
-    <section class="about-me" id="about-me_ancre">
-        <div class="about-me_information">
-            <h2>Qui suis je ?</h2>
-            <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
-            Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p>
-                <div class="about-me_information--button">
-                     <div class="about-me_button">Curriculum Vitae</div> 
-                </div>    
-        </div>
-        <div class="about-me_photo">
-            <img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait">
-        </div>
-        
-    </section>
-    <section ><?php include ('includes/skills/skills.php')?></section>
+            <section class='portfolio' id="projet_ancre">
+            <?php include ('includes/projets/projets.php')?>
+            </section>
 
-    <section class='portfolio' id="projet_ancre">
-    <?php include ('includes/projets/projets.php')?>
-    </section>
+            <section class='contact' id="contact_ancre">
+                <div class="contact_titre"><h3>Contactez moi</h3></div>
+        <!--         <div class="contact_texte"></div> -->
+                <div class="form">
+                <?php include ('includes/contact-form/index.php')?>
+                </div>
+            </section>
 
-    <section class='contact' id="contact_ancre">
-        <div class="contact_titre"><h3>Contactez moi</h3></div>
-<!--         <div class="contact_texte"></div> -->
-        <div class="form">
-        <?php include ('includes/contact-form/index.php')?>
-        </div>
-    </section>
- 
     <?php include ('includes/footer/footer.php')?>
     
   <script src="scripts/main.js"></script>  
