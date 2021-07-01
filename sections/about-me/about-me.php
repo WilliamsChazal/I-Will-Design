125f1000a008a7821c78a41d9dbcbc6cfe807807
williams <w.chazal@codeur.online>
2021-07-01 18:00:53 +0200

    ajout bouton CV dans section about-me

--- sections/about-me/about-me.php
+++ sections/about-me/about-me.php
@@ -3,7 +3,8 @@
 <div class="container_info">
     <div class="texte_info">
       <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
-      Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p></div>
+      Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p>
+      <span><button>CV</button></span></div>
     <div class="image_info"><img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait_info"></div>
  
   
