f589bb7df06cd68abf288feffc7ff3c8a1d42927
williams <w.chazal@codeur.online>
2021-07-02 15:51:05 +0200

    titre modal ok & section about-me ok

--- sections/about-me/about-me.php
+++ sections/about-me/about-me.php
@@ -2,10 +2,14 @@
     <div class="about_titre"><h2>Que suis je ?</h2></div>
 <div class="container_info">
     <div class="texte_info">
-      <p>Hello Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam officiis cum laboriosam ullam corporis assumenda dolores ex illum consectetur accusamus, expedita, perspiciatis illo odit sed sapiente debitis laudantium hic fuga.
-      Amet quia qui aliquam quod, repellendus porro impedit soluta itaque veniam? A adipisci facere temporibus quam culpa aspernatur quibusdam est magni distinctio expedita. Sed vel error modi, sequi et ipsam!</p>
-      <span><button>CV</button></span></div>
-    <div class="image_info"><img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait_info"></div>
+      <p>D'une nature curieuse et entreprenante, après plusieurs années dans le commerce, je me suis lancé le défi de changer de métier. Me voilà désormais webdesigner et développeur web junior. <br>
+       Je conçois et réalise des sites web du cahier des charges à la mise en ligne, en passant par la conception graphique, et le développement. Vous trouverez sur ce Portfolio un aperçu des différents projets que j'ai pu réaliser seul ou en groupe de travail. <br>
+       J’aime les gens passionnés et engagés dans leur travail. J’aime travailler avec eux et les accompagner dans leurs projets.<br>
+      N'hésitez pas à télécharger mon Cv ou à me contacter pour plus d'informations sur mes prestations. 
+       !</p>
+      <span class='about-me_cv'><button id='cv_button'>curriculum vitae</button></span>
+  </div>
+  <div class="image_info"><img src="assets/images/photo.png" alt="Williams Chazal portrait" class="portrait_info"></div>
  
   
 </div>
